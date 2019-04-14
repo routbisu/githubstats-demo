eac8034c320063733c74cf26a50447c2b0126df5
Biswaranjan Rout <biswaranjan@Biswaranjans-MacBook-Air.local>
2019-04-14 19:10:51 +0530

    Updates

---
+++ precache-manifest.d7f466783502f779a58d7836e40ee6f2.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "2a777e61e57272f7160a",
+    "url": "/githubstats-demo/static/js/runtime~main.6ea16de9.js"
+  },
+  {
+    "revision": "6ea8309b9b24f3a5325e",
+    "url": "/githubstats-demo/static/js/main.5f427fa4.chunk.js"
+  },
+  {
+    "revision": "2ef09a951979c527beb2",
+    "url": "/githubstats-demo/static/js/2.6d0c2dee.chunk.js"
+  },
+  {
+    "revision": "6ea8309b9b24f3a5325e",
+    "url": "/githubstats-demo/static/css/main.4843bdec.chunk.css"
+  },
+  {
+    "revision": "d92287d307287a0455dc0689f021041e",
+    "url": "/githubstats-demo/index.html"
+  }
+];
